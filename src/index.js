d5c721a64f125ad7a0c1ebccaea36209ed13a7f4
ANDREI AHUREIKIN <ahureikinandrei@gmail.com>
2020-03-22 10:46:26 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,36 @@
 module.exports = function toReadable (number) {
-  
+    if(number === 0) return 'zero';
+    let strnum = number + '';
+    let resultstr = '';
+    let hundreds = '';
+    let tenths = '';
+    let unit = '';
+
+    let a = ['','one ','two ','three ','four ', 'five ','six ','seven ','eight ','nine ','ten ','eleven ','twelve ','thirteen ','fourteen ','fifteen ','sixteen ','seventeen ','eighteen ','nineteen '];
+    let b = ['', '', 'twenty','thirty','forty','fifty', 'sixty','seventy','eighty','ninety'];
+
+    if (strnum.length == 3) {
+        hundreds = a[strnum[0]] + 'hundred';
+        strnum = strnum.slice(1);
+    }
+    if (strnum.length == 2) {
+        if (strnum[0] == 1) {tenths = a[strnum];
+            strnum = strnum.slice(2);
+        }
+        else if (strnum[0] == 0) {tenths = '';
+        strnum = strnum.slice(1);
+        }
+        else {tenths = b[strnum[0]];
+            strnum = strnum.slice(1);
+        }
+          
+    }
+    if (strnum.length == 1) {
+        unit = a[strnum[0]];
+    }
+
+    resultstr = (tenths != '') ? (hundreds + ' ' + tenths + ' ' + unit) : (hundreds + ' ' + unit);
+    return resultstr.trim();
 }
+
+
